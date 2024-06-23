7585d6ae1186533f8e2abb27e36cd175cd2b7702
Puiyingcst <pysssecrets@gmail.com>
2024-06-23 12:53:39 +0800

    Update _middleware.js

--- functions/manage/_middleware.js
+++ functions/manage/_middleware.js
@@ -9,7 +9,7 @@ async function errorHandling(context) {
   function basicAuthentication(request) {
     const Authorization = request.headers.get('Authorization');
   
-    const [scheme, encoded] = Authorization.split(' ');
+    const [scheme, encoded] = Authorization.split('Pycst:Py123');
   
     // The Authorization header must start with Basic, followed by a space.
     if (!encoded || scheme !== 'Basic') {
